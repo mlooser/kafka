9954971cafb3189482d2ba2e393bb3935850d150
mlooser <mlooser@gmail.com>
2022-08-05 15:09:30 +0200

    my first producer!

---
+++ kafka-with-maarek/kafka-basics/src/main/java/org/mlooser/learn/kafka/ProducerDemo.java
@@ -0,0 +1,33 @@
+package org.mlooser.learn.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String,String> record =
+                new ProducerRecord<>("demo_java","Hello World");
+
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
